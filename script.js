2a89fa1e4d826f85ab05bc5e9537f3d3196ac77a
Jan Minar <rdancer@rdancer.org>
2022-12-16 04:06:28 +0000

    Implement the business logic (buggy)

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
+function gridSize() { return parseInt(getComputedStyle(gameBoard).getPropertyValue("--grid-size"), 10); }
+
 const gameBoard = document.getElementById("game-board");
+const startButton = document.getElementById("start-button");
 
 const pieceTypes = ["I", "O", "T", "S", "Z", "J", "L"];
 
@@ -40,89 +43,224 @@ const pieces = {
     },
     J: {
         shape: [
-            [1, 0, 0],
-            [1, 1, 1],
-            [0, 0, 0]
+            [0, 1, 0],
+            [0, 1, 0],
+            [1, 1, 0]
         ]
     },
     L: {
         shape: [
-            [0, 0, 1],
-            [1, 1, 1],
-            [0, 0, 0]
+            [0, 1, 0],
+            [0, 1, 0],
+            [0, 1, 1]
         ]
     }
 };
 
+let currentPiece;
+let gameBoardArray = [];
+let score = 0;
+let gameOver = false;
+let gameInterval;
 
-const startButton = document.getElementById("start-button");
-startButton.addEventListener("click", startGame);
-
-function gridSize() { return parseInt(getComputedStyle(gameBoard).getPropertyValue("--grid-size"), 10); }
-
-function createPieceElement(type) {
-    const element = document.createElement("div");
-    element.classList.add("block", type);
-    return element;
+// Initialize game board array with empty rows
+for (let i = 0; i < 20; i++) {
+    gameBoardArray.push(new Array(10).fill(0));
 }
 
-function drawPiece(piece) {
-    piece.shape.forEach((row, y) => {
+// Create a new random piece
+function createPiece() {
+    let type = pieceTypes[Math.floor(Math.random() * pieceTypes.length)];
+    currentPiece = {
+        type: type,
+        x: 4,
+        y: 0,
+        shape: pieces[type].shape
+    };
+}
+
+// Draw the current piece on the game board
+function drawPiece() {
+    // Remove any existing block elements
+    while (gameBoard.firstChild) {
+        gameBoard.removeChild(gameBoard.firstChild);
+    }
+
+    // Add block elements for each block in the piece
+    currentPiece.shape.forEach((row, y) => {
         row.forEach((value, x) => {
             if (value === 1) {
-                const block = createPieceElement(piece.type);
-                block.style.left = ((piece.position[1] + y) * gridSize()) + "px";
-                block.style.top = ((piece.position[0] + x) * gridSize()) + "px";
+                let block = document.createElement("div");
+                block.classList.add("block");
+                block.classList.add(currentPiece.type);
+                block.style.top = `${currentPiece.y + y}00%`;
+                block.style.left = `${currentPiece.x + x}00%`;
                 gameBoard.appendChild(block);
             }
         });
     });
 }
 
-function movePieceLeft() {
-    // Update the position of the piece
+// Check if the current piece can move down by one row
+function canMoveDown() {
+    let canMove = true;
+    currentPiece.shape.forEach((row, y) => {
+        row.forEach((value, x) => {
+            if (value === 1) {
+                let newY = currentPiece.y + y + 1;
+                if (newY >= 20 || gameBoardArray[newY][currentPiece.x + x] === 1) {
+                    canMove = false;
+                }
+            }
+        });
+    });
+    return canMove;
 }
 
-function movePieceRight() {
-    piece.position[1] += 1;
-    const blocks = document.querySelectorAll(".block");
-    blocks.forEach(block => {
-        block.style.left = (block.offsetLeft + gridSize()) + "px";
+// Move the current piece down by one row
+function movePieceDown() {
+    if (canMoveDown()) {
+        currentPiece.y++;
+    } else {
+        addPieceToBoard();
+        createPiece();
+        if (checkGameOver()) {
+            gameOver = true;
+        }
+    }
+    drawPiece();
+}
+
+// Rotate the current piece clockwise
+function rotatePiece() {
+    let newShape = transpose(currentPiece.shape);
+    newShape = flip(newShape);
+    if (checkCollision(newShape)) {
+        currentPiece.shape = newShape;
+    }
+    drawPiece();
+}
+
+// Transpose a matrix (turn rows into columns and columns into rows)
+function transpose(matrix) {
+    let newMatrix = [];
+    for (let x = 0; x < matrix[0].length; x++) {
+        newMatrix.push([]);
+        for (let y = 0; y < matrix.length; y++) {
+            newMatrix[x].push(matrix[y][x]);
+        }
+    }
+    return newMatrix;
+}
+
+// Flip a matrix horizontally
+function flip(matrix) {
+    return matrix.map(row => row.reverse());
+}
+
+// Check if the current piece collides with any other pieces or the edges of the game board
+function checkCollision(shape) {
+    let collision = false;
+    shape.forEach((row, y) => {
+        row.forEach((value, x) => {
+            if (value === 1) {
+                if (
+                    currentPiece.x + x < 0 ||
+                    currentPiece.x + x > 9 ||
+                    currentPiece.y + y < 0 ||
+                    currentPiece.y + y > 19 ||
+                    gameBoardArray[currentPiece.y + y][currentPiece.x + x] === 1
+                ) {
+                    collision = true;
+                }
+            }
+        });
+    });
+    return !collision;
+}
+
+// Add the current piece to the game board array
+function addPieceToBoard() {
+    currentPiece.shape.forEach((row, y) => {
+        row.forEach((value, x) => {
+            if (value === 1) {
+                gameBoardArray[currentPiece.y + y][currentPiece.x + x] = 1;
+            }
+        });
     });
 }
 
-function movePieceDown() {
-    // Update the position of the piece
+// Check if the game is over
+function checkGameOver() {
+    return gameBoardArray[0].some(value => value === 1);
+}
+// Update the game state and redraw the game board
+function gameLoop() {
+    movePieceDown();
+    checkRows();
+    drawBoard();
+    if (gameOver) {
+        clearInterval(gameInterval);
+        startButton.style.display = "block";
+    }
 }
 
-function rotatePiece() {
-    // Rotate the shape of the piece
+// Check for completed rows and remove them
+function checkRows() {
+    for (let y = gameBoardArray.length - 1; y >= 0; y--) {
+        if (gameBoardArray[y].every(value => value === 1)) {
+            gameBoardArray.splice(y, 1);
+            gameBoardArray.unshift(new Array(10).fill(0));
+            score++;
+        }
+    }
 }
 
-// Implement the game logic
-function updateGame() {
-    // Check for completed rows and remove them
-    // Check for game over conditions
-    // Update the score
+// Draw the game board
+function drawBoard() {
+    gameBoardArray.forEach((row, y) => {
+        row.forEach((value, x) => {
+            if (value === 1) {
+                let block = document.createElement("div");
+                block.classList.add("block");
+                block.style.top = `${y}00%`;
+                block.style.left = `${x}00%`;
+                gameBoard.appendChild(block);
+            }
+        });
+    });
 }
 
-function startGame() {
+// Handle keyboard input
+document.addEventListener("keydown", event => {
+    switch (event.keyCode) {
+        case 37: // Left arrow
+            if (currentPiece.x > 0 && checkCollision(currentPiece.shape, -1)) {
+                currentPiece.x--;
+            }
+            break;
+        case 39: // Right arrow
+            if (currentPiece.x < 9 && checkCollision(currentPiece.shape, 1)) {
+                currentPiece.x++;
+            }
+            break;
+        case 40: // Down arrow
+            if (canMoveDown()) {
+                movePieceDown();
+            }
+            break;
+        case 38: // Up arrow
+            rotatePiece();
+            break;
+    }
+    drawPiece();
+});
 
-    // Clear the game board
-    gameBoard.innerHTML = "";
-
-
-    // Generate a random piece
-    const randomType = pieceTypes[Math.floor(Math.random() * pieceTypes.length)];
-    const randomPiece = {
-        position: [0, 4],
-        ...pieces[randomType],
-        type: randomType
-    };
-
-    // Add the piece to the game board
-    drawPiece(randomPiece);
-}
-
-
+// Start the game
+startButton.addEventListener("click", () => {
+    //startButton.style.display = "none";
+    createPiece();
+    drawPiece();
+    gameInterval = setInterval(gameLoop, 1000);
+});
 
