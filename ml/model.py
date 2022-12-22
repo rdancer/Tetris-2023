c4233a68368cce4554b1c60dd27bd5522de5da04
Jan Minar <rdancer@rdancer.org>
2022-12-22 18:38:44 +0000

    get started on model training

--- ml/model.py
+++ ml/model.py
@@ -1,1 +1,42 @@
 # model.py -- This module contains the code for training and using a reinforcement learning model to play Tetris. It defines a Model class that defines the model and provides methods for training and using the model to take actions in the game.
+
+import tensorflow as tf
+
+# Define the state space.
+def get_state():
+  return Control.getState()
+
+# Define the action space.
+def get_actions():
+  return [Control.drop, Control.left, Control.right, Control.down, Control.rotate]
+
+# Define the reward function.
+def get_reward():
+  return Control.getHighScore() - Control.getScore()
+
+# Create a Q-learning model.
+model = tf.keras.Sequential()
+model.add(tf.keras.layers.Dense(64, activation='relu', input_shape=(state_size,)))
+model.add(tf.keras.layers.Dense(64, activation='relu'))
+model.add(tf.keras.layers.Dense(num_actions))
+
+# Compile the model.
+model.compile(optimizer='adam', loss='mse')
+
+# Train the model.
+for i in range(num_iterations):
+  # Get the current state of the game.
+  state = get_state()
+
+  # Choose an action.
+  action = np.argmax(model.predict(state)[0])
+
+  # Take the action.
+  actions[action]()
+
+  # Observe the resulting game state and reward.
+  next_state = get_state()
+  reward = get_reward()
+
+  # Update the model.
+  model.fit(state, reward, epochs=1)
